dfbaaf89605b935c3d1c2d7e0fbf6609dcf6b80b
lilia <143701282+lrdinsu@users.noreply.github.com>
2024-01-10 04:54:57 -0800

    feat: add Prisma migrations and schemas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,32 @@
 import express, { Express } from 'express';
+import * as process from 'process';
+
+import { PrismaClient } from '@prisma/client';
 
 export const app: Express = express();
 
 app.use(express.json());
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.findUnique({
+    where: {
+      age_name: {
+        age: 24,
+        name: 'Kyle',
+      },
+    },
+  });
+  console.log(user);
+  process.exit(0);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
